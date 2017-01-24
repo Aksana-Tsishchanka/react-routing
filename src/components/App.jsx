b66b4a7d7eccccbcd58d53d0cf007961243c8212
Aksana Tolstoguzova <aksana_tolstoguzova@Aksanas-MacBook-Pro.local>
2017-01-24 23:09:29 +0300

    Server can be run, fetch is added- draft version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,17 +15,51 @@ export default class App extends React.Component {
    }
 }
 
-class Login extends React.Component {
+export class Login extends React.Component {
+   constructor(props) {
+      super(props);
+      
+      this.state = {
+         login : '',
+         password : '',
+      }
+      this.onChangeInput = this.onChangeInput.bind(this);
+      this.onLogin = this.onLogin.bind(this);
+
+   };
+   onChangeInput({ target : {value, name} }) {
+      this.setState({
+         [name]: value,
+      })
+   }
+   onLogin(e) {
+      e.preventDefault();
+      const init = {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(this.state),
+      }   
+      sendRequest('http://localhost:3000/login', init);
+   }
+
    render() {
       return (
          <div>
             <h1>Login page...</h1>
+            <form>
+               User name: <input type="text" name="login" value={this.state.login} onChange={this.onChangeInput} />
+               Password: <input type="text" name="password" value={this.state.password} 
+               onChange={this.onChangeInput} />
+               <button onClick = {this.onLogin}>Log in</button>
+            </form> 
          </div>
       )
    }
 }
 
-class TodoList extends React.Component {
+export class TodoList extends React.Component {
    render() {
       return (
          <div>
@@ -33,4 +67,14 @@ class TodoList extends React.Component {
          </div>
       )
    }
+}
+
+function processJson(json) {
+  console.log(json);
+}
+
+export function sendRequest(url, init) {
+   fetch(url, init)
+  .then(response => response.json())
+  .then(processJson);
 }
--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App, {Login, TodoList} from './App.jsx';
-import { Router, Route, browserHistory, IndexRoute  } from 'react-router';
+import { Router, Route, browserHistory, hashHistory, IndexRoute  } from 'react-router';
 
-const router = (
-   <Router history = {browserHistory}>
-      <Route path = "/" component = {App}>
-         <IndexRoute component = {App} />
-         <Route path = "login" component = {Login} />
-         <Route path = "todo" component = {TodoList} />
+ReactDOM.render((
+   <Router history={browserHistory}>
+      <Route path = "/" component = { App }>
+         { /*<IndexRoute component = { Login } /> */}
+         <Route path = "/login" component = {Login} />
+         <Route path = "/todo" component = {TodoList} />
       </Route>
-   </Router>
-	
-);
-
-ReactDOM.render(router, document.getElementById('app'));
+   </Router>	
+), document.getElementById('app'));
